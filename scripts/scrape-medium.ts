fc9a7dfc630ef5cc90399192c5fa83c2ff1e196e
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2024-01-22 21:42:35 +0100

    optimize scraping script

--- scripts/scrape-medium.ts
+++ scripts/scrape-medium.ts
@@ -1,59 +1,84 @@
-import fs from 'node:fs/promises'
+import fs from 'node:fs/promises';
 import { load } from 'cheerio';
 
-const fetchHtml = async (url: string) => {
-  const response = await fetch(url);
-  const html = await response.text();
+const dataDir = 'src/data';
 
-  return html;
-}
+const fetchHtml = async (url: string) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.text();
+  } catch (error) {
+    console.error(`Error fetching HTML from ${url}:`, error);
+    throw error;
+  }
+};
 
 const handleImage = async (imgUrl: string) => {
-  // write image to disk and remove all formatting params
-  const splitUrl = imgUrl.split('/');
-  const imageName = splitUrl[splitUrl.length - 1].split('?')[0];
-  const originalImageUrl = imgUrl.replace('resize:fill:224:224/', '')
+  try {
+    const imageName = imgUrl.split('/').pop()?.split('?')[0] || '';
+    const originalImageUrl = imgUrl.replace('resize:fill:224:224/', '');
 
-  // download image
-  const imageResponse = await fetch(originalImageUrl);
-  const imageBuffer = await imageResponse.arrayBuffer();
+    const response = await fetch(originalImageUrl);
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
 
-  await fs.mkdir('src/data/images', { recursive: true });
+    const imageBuffer = await response.arrayBuffer();
 
-  // write image to disk
-  await fs.writeFile(`src/data/images/${imageName}`, Buffer.from(imageBuffer));
+    const imagesDir = 'src/data/images';
+    await fs.mkdir(imagesDir, { recursive: true });
 
-  return imageName
-}
+    const imagePath = `${imagesDir}/${imageName}`;
+    await fs.writeFile(imagePath, Buffer.from(imageBuffer));
+
+    return imageName;
+  } catch (error) {
+    console.error(`Error handling image ${imgUrl}:`, error);
+    throw error;
+  }
+};
 
 const scrapeArticles = async () => {
-  const url = 'https://medium.com/@sjoerd3000';
-  const html = await fetchHtml(url);
+  try {
+    const url = 'https://medium.com/@sjoerd3000';
+    const html = await fetchHtml(url);
+    const $ = load(html);
+    const articleElements = $('article')
 
-  const $ = load(html);
+    const articles = await Promise.all(articleElements.map(async (_, element) => {
+      const title = $(element).find('h2').text();
+      const excerpt = $($(element).find('p').get(1)).text();
+      const path = $(element).find('a').attr('href');
+      const articleUrl = `https://medium.com${path?.split('?')[0]}`;
+      const imgUrl = $(element).find('img').attr('src');
+  
+      if (imgUrl) {
+        const imageName = await handleImage(imgUrl);
+  
+        return {
+          url: articleUrl,
+          title,
+          excerpt,
+          imageName,
+        };
+      } else {
+        return {
+          url: articleUrl,
+          title,
+          excerpt,
+        };
+      }
+    }));
 
-  const headings = $('article');
+    await fs.mkdir(dataDir, { recursive: true });
+    await fs.writeFile(`${dataDir}/articles.json`, JSON.stringify(articles, null, 2));
+  } catch (error) {
+    console.error('Error scraping articles:', error);
+  }
+};
 
-  const articles = await Promise.all(headings.map(async (_, element) => {
-    const title = $(element).find('h2').text();
-    const excerpt = $($($(element).find('p').toArray()[1]).toString()).text();
-    const path = $(element).find('a').attr('href');
-    const url = `https://medium.com${path?.split('?')[0]}`;
-    const imgUrl = $(element).find('img').attr('src')
-
-    const imageName = await handleImage(imgUrl || '');
-
-    return {
-      url,
-      title,
-      excerpt,
-      imageName
-    }
-  }));
-
-  await fs.mkdir('src/data', { recursive: true });
-
-  await fs.writeFile('src/data/articles.json', JSON.stringify(articles, null, 2));
-}
-
-scrapeArticles();
+scrapeArticles().then(() => console.log('Scraping completed.'));
